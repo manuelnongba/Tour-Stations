26cd290d14afd7e7cab6221a32a9fed0487dbcc2
manuelnongba <eanongba19@gmail.com>
2022-10-04 23:48:50 +0000

    Tour Destinations

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorControllers');
 const tourRouter = require('./routes/tourRoutes');
-
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
@@ -19,10 +19,10 @@ app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middlewware 👋');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middlewware 👋');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -53,6 +53,33 @@ app.use((req, res, next) => {
 //3) routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+// app.use((err, req, res, next) => {
+//   console.log(err.stack);
+
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || `error`;
+
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+//   });
+// });
+
+app.use(globalErrorHandler);
 
 //4) Start server
 module.exports = app;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -17,6 +17,7 @@ const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/tour-stats').get(getTourStats);
+
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
